cf21634544a22f58c400e4510c77b5ab62211842
asmashaik040 <asmashaik040@gmail.com>
2024-05-23 15:35:10 +0530

    Personal Front End Developer Portfolio

---
+++ assets/js/portfolio.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	"use strict";
+    
+    $(window).on('scroll',function () {
+        if ($(this).scrollTop() > 600) {
+            $('.return-to-top').fadeIn();
+        } else {
+            $('.return-to-top').fadeOut();
+        }
+    });
+    $('.return-to-top').on('click',function(){
+            $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});	
